ab385c661669ef90154d516691f506ac54f219c8
Ahmet Volkan Turan <a.volkan2004@gmail.com>
2026-03-10 11:50:14 -0400

    Added Serializable EnvironmentData Class. Added Read and Write Functions for the NFC

---
+++ app/src/main/java/edu/fandm/volkanwill/meebles/EnvironmentData.java
@@ -0,0 +1,50 @@
+package edu.fandm.volkanwill.meebles;
+
+import java.io.Serializable;
+import java.time.LocalTime;
+
+public class EnvironmentData implements Serializable {
+
+    private int meebleCount;
+    private char cityType;
+    private char environmentType;
+    private LocalTime time;
+
+    public EnvironmentData(int meebleCount, char cityType, char environmentType, LocalTime time){
+        this.meebleCount = meebleCount;
+        this.cityType = cityType;
+        this.environmentType = environmentType;
+        this.time = time;
+    }
+
+    public int getMeebleCount(){
+        return meebleCount;
+    }
+
+    public char getCityType(){
+        return cityType;
+    }
+
+    public char getEnvironmentType(){
+        return environmentType;
+    }
+
+    public LocalTime getTime(){
+        return time;
+    }
+
+    public void setMeebleCount(int meebleCount){
+        this.meebleCount = meebleCount;
+    }
+
+    public void setCityType(char CityType){
+        this.cityType=cityType;
+    }
+
+    public void setEnvironmentType(char environmentType){
+        this.environmentType=environmentType;
+    }
+    public void setTime(LocalTime time){
+        this.time = time;
+    }
+}
--- app/src/main/java/edu/fandm/volkanwill/meebles/EnvironmentPage.java
+++ app/src/main/java/edu/fandm/volkanwill/meebles/EnvironmentPage.java
@@ -1,8 +1,17 @@
 package edu.fandm.volkanwill.meebles;
 
 import android.content.Intent;
+import android.media.audiofx.EnvironmentalReverb;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -10,6 +19,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButtonToggleGroup;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.time.LocalTime;
 
 public class EnvironmentPage extends AppCompatActivity {
 
@@ -23,6 +40,8 @@ public class EnvironmentPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        adapter = NfcAdapter.getDefaultAdapter(this);
 
         ImageButton go_back = (ImageButton) findViewById(R.id.environment_go_back_button);
         go_back.setOnClickListener(new View.OnClickListener() {
@@ -41,5 +60,123 @@ public class EnvironmentPage extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Bundle options = new Bundle();
+        MYNFCCallBackClass myCallback = new MYNFCCallBackClass();
+        adapter.enableReaderMode(this, myCallback, NfcAdapter.FLAG_READER_NFC_A, options);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        adapter.disableReaderMode(this);
+    }
+
+    private class MYNFCCallBackClass implements NfcAdapter.ReaderCallback {
+        @Override
+        public void onTagDiscovered(Tag tag){
+            Log.d("", "Tag Discovered: " + tag.toString());
+            EditText et = (EditText) findViewById(R.id.nfc_number_et);
+            int value = Integer.parseInt(et.getText().toString());
+            EnvironmentData data = readFromTag(tag);
+
+            MaterialButtonToggleGroup mbtg = (MaterialButtonToggleGroup) findViewById(R.id.withdraw_deposit_toggle);
+            int selectedId = mbtg.getCheckedButtonId();
+            if(selectedId == R.id.withdraw){
+                if(value < data.getMeebleCount()) {
+                    data.setMeebleCount(data.getMeebleCount() - value);
+                }
+            } else if(selectedId == R.id.deposit){
+                data.setMeebleCount(data.getMeebleCount()+value);
+            }
+
+            writeToTag(data, tag);
+        }
+    }
+
+
+    private void writeToTag(EnvironmentData data, Tag tag) {
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(data);
+            oos.flush();
+            byte[] serializedData = bos.toByteArray();
+
+            String mimeType = "application/edu.fandm.volkanwill.meebles.environmentdata";
+            NdefRecord record = NdefRecord.createMime(mimeType, serializedData);
+
+
+            NdefMessage message = new NdefMessage(new NdefRecord[]{record});
+
+            formatAndWriteToTag(tag, message);
+
+            oos.close();
+            bos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void formatAndWriteToTag(Tag tag, NdefMessage message) {
+        Ndef ndef = Ndef.get(tag);
+
+        try {
+            if (ndef != null) {
+                ndef.connect();
+                if (!ndef.isWritable()) {
+                    return;
+                }
+                if (ndef.getMaxSize() < message.getByteArrayLength()) {
+                    return;
+                }
+                ndef.writeNdefMessage(message);
+            } else {
+                NdefFormatable formattable = NdefFormatable.get(tag);
+                if (formattable != null) {
+                    formattable.connect();
+                    formattable.format(message);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private EnvironmentData readFromTag(Tag tag) {
+        Ndef ndef = Ndef.get(tag);
+        EnvironmentData data = new EnvironmentData(0,'0','0',LocalTime.now());
+        try {
+            ndef.connect();
+            NdefMessage msg = ndef.getNdefMessage();
+            if (msg != null && msg.getRecords().length > 0) {
+                byte[] payload = msg.getRecords()[0].getPayload();
+                try (ByteArrayInputStream bis = new ByteArrayInputStream(payload);
+                     ObjectInputStream ois = new ObjectInputStream(bis)) {
+                    EnvironmentData data = (EnvironmentData) ois.readObject();
+                    return data;
+                }
+                catch (ClassNotFoundException | ClassCastException e) {
+                    Log.e("NFC_READ", "Class mismatch or not found", e);
+                }
+            }
+            else {
+                Log.e("NFC_READ", "Tag is empty.");
+            }
+        } catch (Exception e) {
+            // Handle read error
+            e.printStackTrace();
+        }finally {
+            try {
+                ndef.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return data;
     }
 }
