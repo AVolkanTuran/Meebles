5ecb3c1bd9f690e4806510a55d38eaefd8275fa4
Will Nguyen <mdnguyen123321@gmail.com>
2026-03-10 11:49:22 -0400

    Added welcomepage, logo and button. Added homepage with a simple button to the tutorial page

--- app/src/main/java/edu/fandm/volkanwill/meebles/HomePage.java
+++ app/src/main/java/edu/fandm/volkanwill/meebles/HomePage.java
@@ -1,6 +1,13 @@
 package edu.fandm.volkanwill.meebles;
 
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +15,56 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButtonToggleGroup;
+
 public class HomePage extends AppCompatActivity {
+
+    public static NfcAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_welcome_page);
+        //EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        adapter = NfcAdapter.getDefaultAdapter(this);
+
+        Button tutorial_button = findViewById(R.id.tutorial_button);
+
+        tutorial_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), TutorialPage.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Bundle options = new Bundle();
+        MYNFCCallbackClass myCallback = new MYNFCCallbackClass();
+        adapter.enableReaderMode(this, myCallback, NfcAdapter.FLAG_READER_NFC_A,options);
+    }
+
+    @Override
+    protected  void onPause() {
+        super.onPause();
+        adapter.disableReaderMode(this);
+    }
+    private class MYNFCCallbackClass implements NfcAdapter.ReaderCallback {
+        @Override
+        public void onTagDiscovered(Tag tag) {
+            Intent i = new Intent(getApplicationContext(), EnvironmentPage.class);
+            startActivity(i);
+        }
+
     }
 }
--- app/src/main/java/edu/fandm/volkanwill/meebles/WelcomePage.java
+++ app/src/main/java/edu/fandm/volkanwill/meebles/WelcomePage.java
@@ -1,6 +1,12 @@
 package edu.fandm.volkanwill.meebles;
 
+import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Arrays;
+
 public class WelcomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button b = findViewById(R.id.start_button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), HomePage.class);
+                startActivity(i);
+            }
         });
     }
 }
