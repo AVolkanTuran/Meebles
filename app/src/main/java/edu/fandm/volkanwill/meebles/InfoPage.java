a579718d299f573064d2414cbe9c7d095a30774e
Will Nguyen <mdnguyen123321@gmail.com>
2026-03-14 14:18:55 -0400

    added stuff to the info page

--- app/src/main/java/edu/fandm/volkanwill/meebles/InfoPage.java
+++ app/src/main/java/edu/fandm/volkanwill/meebles/InfoPage.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -39,6 +40,93 @@ public class InfoPage extends AppCompatActivity {
                 finish();
             }
         });
+
+        TextView infoText = findViewById(R.id.info_text);
+
+        int biome = getIntent().getIntExtra("biome", MeebleConstants.ENV_FOREST);
+        int level = getIntent().getIntExtra("level", MeebleConstants.VILLAGE);
+        double growthRate;
+
+        switch(biome){
+            case MeebleConstants.ENV_VOLCANO:
+                growthRate = 0.5;
+                break;
+            case MeebleConstants.ENV_FOREST:
+                growthRate = 2.0;
+                break;
+            case MeebleConstants.ENV_DESERT:
+                growthRate = 1.0;
+                break;
+            case MeebleConstants.ENV_TUNDRA:
+                growthRate = 1.5;
+                break;
+            default:
+                growthRate = 1.0;
+        }
+
+        String biomeMessage = "";
+        String nextCheckpoint = "";
+
+        switch (biome) {
+            case MeebleConstants.ENV_FOREST:
+                biomeMessage = "• Forests are nice to live in. The meebles will grow quickly.";
+                break;
+
+            case MeebleConstants.ENV_TUNDRA:
+                biomeMessage = "• Tundras are a little chilly. It's a little rough for the meebles.";
+                break;
+
+            case MeebleConstants.ENV_DESERT:
+                biomeMessage = "• No heat and water means less fun for the meebles.";
+                break;
+
+            case MeebleConstants.ENV_VOLCANO:
+                biomeMessage = "• Is it just me or is it really hot in there?";
+                break;
+        }
+
+        switch (level) {
+            case MeebleConstants.VILLAGE:
+                nextCheckpoint = "town";
+                break;
+
+            case MeebleConstants.TOWN:
+                nextCheckpoint = "city";
+                break;
+
+            case MeebleConstants.CITY:
+                nextCheckpoint = "You've mastered exponential growth!";
+                break;
+        }
+
+        String growthMessage = "";
+
+        if(level == MeebleConstants.CITY){
+            growthMessage = "• Growth rate: " + growthRate +
+                    ". Your meebles are growing incredibly fast. " + nextCheckpoint;
+        }
+        else if (level == MeebleConstants.TOWN){
+            growthMessage = "• Growth rate: " + growthRate +
+                    ". At this rate your town will become a " + nextCheckpoint + " soon!";
+        }
+        else if (level == MeebleConstants.VILLAGE){
+            growthMessage = "• Growth rate: " + growthRate +
+                    ". It will take a while for your village to reach a " + nextCheckpoint;
+        }
+
+        String plotMessage =
+                "• Notice how the population starts slow " +
+                        "but increases faster and faster over time. Each generation multiplies the number of meebles!";
+
+        String fullText = biomeMessage + "\n\n" +
+                growthMessage + "\n\n" +
+                plotMessage;
+
+        infoText.setText(fullText);
+
+        PopulationPlotView plotView = findViewById(R.id.population_plot);
+        plotView.setGrowthRate(growthRate);
+        plotView.startDotAnimation();
     }
 
     //Added this code to ignore scanning
